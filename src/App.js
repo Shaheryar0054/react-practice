854d2837da6e12ad490aad4eb6755ffc3257a9d0
Shaheryar0054 <sharyar0310@gmail.com>
2023-05-09 11:38:22 +0100

    Added counter function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import List from './List';
 import State from './State';
+import Counter from './Counter';
 
 function App() {
   return (
     <div>
       <List />
       <State />
+      <Counter />
     </div>
   );
 }
