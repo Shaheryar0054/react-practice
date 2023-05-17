bdf71faf0fea11c8fdf9c06a0cfe001304f9b105
Shaheryar0054 <sharyar0310@gmail.com>
2023-05-17 19:53:28 +0100

    Created a new file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import List from './List';
-import State from './State';
-import Counter from './Counter';
-import LifeCycle from './LifeCycle';
-import Usedstate from './Usedstate';
 
 function App() {
   return (
-    <div>
-      <List />
-      <State />
-      <Counter />
-      <LifeCycle />
-      <Usedstate />
-    </div>
+    <div>App</div>
   );
 }
 
